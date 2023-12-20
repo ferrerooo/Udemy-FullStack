d1b380080d3181deaea4c2aac86bdbc683b39008
ferrerooo <ferrerooo@gmail.com>
2023-12-20 00:12:01 -0800

    update

--- 2.1 Using Node/index.js
+++ 2.1 Using Node/index.js
@@ -1,1 +1,24 @@
 console.log("Hello from Node!")
+
+async function test() {
+    function testAsync() {
+        return new Promise(function(resolve1, reject) {
+            setTimeout(function() {
+                if (true) {
+                    console.log('requesting...')
+                    resolve1('resolve return')
+                } else {
+                    reject('reject return')
+                }
+            }, 1000)
+        })
+    }
+    var result =  testAsync();
+    var result1 = "testAsync后执行";
+    console.log(result);
+    console.log(result1);
+}
+
+test()
+
+console.log("Ending...")
